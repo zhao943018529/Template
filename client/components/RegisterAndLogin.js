cb68e7dc6872414f7fc765a09ca2519498501fb0
zhao943018529 <943018529@qq.com>
2018-08-16 22:19:36 +0800

    change component state model

--- client/components/RegisterAndLogin.js
+++ client/components/RegisterAndLogin.js
@@ -3,25 +3,37 @@ import {Map} from 'immutable';
 import {fetchData} from '../utilities/fetch';
 import LightTip from '../controls/LightTip';
 
+const LOGIN_TYPE ="login";
+
+const REGISTER_URL='/api/register';
+const VALI_URL='/api/valiUsername';
+
 export default class RegisterAndLogin extends React.Component{
     constructor(props){
         super(props);
-
+        //1loading 2success 3error
         this.state={
-            values:Map({
-                username:'',
-                nickname:'',
-                password:'',
-                phone:'',
-                email:'',
-            }),
-            validation:Map({
-                username:'',
-                nickname:'',
-                password:'',
-                phone:'',
-                email:'',
-            }),
+                username:Map({
+                    value:'',
+                    validation:'',
+                }),
+                nickname:Map({
+                    value:'',
+                    validation:'',
+                }),
+                password:Map({
+                    value:'',
+                    validation:'',
+                }),
+                phone:Map({
+                    value:'',
+                    validation:'',
+                }),
+                email:Map({
+                    value:'',
+                    validation:'',
+                }),
+                status:0,
         };
         this.submitEventCallback =this._submitEventCallback.bind(this);
         this.registerSuccess = this._registerSuccess.bind(this);
@@ -48,24 +60,24 @@ export default class RegisterAndLogin extends React.Component{
         console.log(data);
     }
 
-    handleValueChange(key,value){
+    handleValueChange(key, value) {
+        let change = this.state[key].set('value', value);
         this.setState({
-            
-        })
+            [key]: change,
+        });
     }
 
     createFormForLogin() {
-        let { username: un_v, password: pw_v } = this.state.validation;
-        let { username, password } = this.state.values;
+        let { username, password } = this.state;
         let className = "form-control";
         let invaliClassName = "form-control is-invalid";
         let username = (
             <div className="form-group">
                 <label htmlFor="username-r">Username</label>
-                <input type="text" name="username" onBlur={} className={un_v ? invaliClassName : className}
-                    value={username} id="username-r" aria-describedby="usernameHelp" placeholder="username" />
+                <input type="text" name="username" onChange={this.handleValueChange.bind(this,'username')} className={username.validation ? invaliClassName : className}
+                    value={username.value} id="username-r" aria-describedby="usernameHelp" placeholder="username" />
                 <div className="invalid-feedback">
-                    {un_v}
+                    {username.validation}
                 </div>
             </div>
         );
@@ -73,9 +85,9 @@ export default class RegisterAndLogin extends React.Component{
         let password = (
             <div className="form-group">
                 <label htmlFor="password-r">Your Password</label>
-                <input type="password" name="password" value={password} className={pw_v ? invaliClassName : className} id="password-r" placeholder="Password" />
+                <input type="password" name="password" value={password.value} onChange={this.handleValueChange.bind(this,'password')} className={password.validation ? invaliClassName : className} id="password-r" placeholder="Password" />
                 <div className="invalid-feedback">
-                    {pw_v}
+                    {password.validation}
                 </div>
             </div>
         );
@@ -84,34 +96,33 @@ export default class RegisterAndLogin extends React.Component{
     }
 
     createFormForRegister() {
-        let { nickname: nn_v, phone: p_v, email: e_v } = this.state.validation;
-        let { nickname, phone, email } = this.state.values;
+        let { nickname, phone, email } = this.state;
         let className = "form-control";
         let invaliClassName = "form-control is-invalid";
         let nickname = (
             <div className="form-group">
                 <label htmlFor="nickname-r">Your Nickname</label>
-                <input type="text" name="nickname" value={nickname} className={nn_v ? invaliClassName : className} id="nickname-r" placeholder="your name or your nickname" />
+                <input type="text" name="nickname" value={nickname.value} onChange={this.handleValueChange.bind(this,'nickname')} className={nickname.validation ? invaliClassName : className} id="nickname-r" placeholder="your name or your nickname" />
                 <div className="invalid-feedback">
-                    {nn_v}
+                    {nickname.validation}
                 </div>
             </div>
         );
         let phone = (
             <div className="form-group">
                 <label htmlFor="phone-r">Your Phone</label>
-                <input type="number" name="phone" value={phone} className={p_v ? invaliClassName : className} id="phone-r" aria-describedby="phoneHelp" placeholder="phone number" />
+                <input type="number" name="phone" value={phone.value} onChange={this.handleValueChange.bind(this,'phone')} className={phone.validation ? invaliClassName : className} id="phone-r" aria-describedby="phoneHelp" placeholder="phone number" />
                 <div className="invalid-feedback">
-                    {nn_v}
+                    {phone.validation}
                 </div>
             </div>
         );
         let email = (
             <div className="form-group">
                 <label htmlFor="email-r">Your Email</label>
-                <input type="email" name="email" value={email} className={e_v ? invaliClassName : className} id="email-r" aria-describedby="emailHelp" placeholder="email" />
+                <input type="email" name="email" value={email.value} onChange={this.handleValueChange.bind(this,'email')} className={email.validation ? invaliClassName : className} id="email-r" aria-describedby="emailHelp" placeholder="email" />
                 <div className="invalid-feedback">
-                    {e_v}
+                    {email.validation}
                 </div>
             </div>
         );
@@ -124,19 +135,17 @@ export default class RegisterAndLogin extends React.Component{
     }
 
     render() {
+        let content;
+        if(this.props.type===LOGIN_TYPE){
+            content = this.createFormForLogin();
+        }else{
+            content = this.createFormForRegister();
+        }
+
         return (
             <form onSubmit={this.submitEventCallback}>
-                <div className="form-group">
-                    <label htmlFor="username-r">Username</label>
-                    <input type="text" name="username" onBlur={} className="form-control" id="username-r" aria-describedby="usernameHelp" placeholder="username" />
-                    <small id="usernameHelp" className="form-text text-muted">.....</small>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password-r">Your Password</label>
-                    <input type="password" name="password" className="form-control" id="password-r" placeholder="Password" />
-                </div>
-
-                <button type="submit" className="btn btn-primary">Submit</button>
+                {content}
+                <button type="submit" disabled={this.state.status===1} className="btn btn-primary">Submit</button>
             </form>
         );
     }

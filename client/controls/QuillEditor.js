b71aaddcfcc3b73a963d86214a30642f8fb9acc0
zhao943018529 <943018529@qq.com>
2018-08-23 22:47:29 +0800

    waste time

--- client/controls/QuillEditor.js
+++ client/controls/QuillEditor.js
@@ -1,4 +1,5 @@
 import React from "react";
+import _ from 'lodash';
 import LightTip from "../controls/LightTip";
 import { fetchData } from "../utilities/fetch";
 import hljs from "highlight.js/lib/highlight";
@@ -34,17 +35,20 @@ export default class QuillEditor extends React.Component {
         this.quillEditor = null;
         this.state = {
             status: 0,
-            message: ""
+            value: props.value,
+            message: "",
         };
         this.uploadSuccess = this._uploadSuccess.bind(this);
         this.uploadFailed = this._uploadFailed.bind(this);
         this.saveToServer = this._saveToServer.bind(this);
+        this.onTextHandler = this.onTextHandler.bind(this);
     }
 
     componentDidMount() {
         hljs.registerLanguage("javascript", javascript);
         let current = this.editorRef.current;
         if (current) {
+            window.katex = katex;
             this.quillEditor = new Quill(current, {
                 modules: {
                     toolbar: toolbarOptions,
@@ -59,33 +63,87 @@ export default class QuillEditor extends React.Component {
             this.quillEditor
                 .getModule("toolbar")
                 .addHandler("image", this.imageHandler(this.saveToServer));
+            if (this.state.value) {
+                this.quillEditor.setContents(this.state.value);
+            }
+            this.quillEditor.on('text-change', this.onTextHandler);
         }
     }
 
-    imageHandler(cb) {
-        return function() {
-          let fileInput = this.container.querySelector("input.ql-image[type=file]");
-          if (!fileInput) {
-            fileInput = document.createElement("input");
-            fileInput.setAttribute("type", "file");
-            fileInput.classList.add("ql-image");
-            fileInput.setAttribute("accept", "image/png, image/gif, image/jpeg, image/bmp, image/x-icon");
-            fileInput.onchange = () => {
-              let file = fileInput.files[0];
-              if (/^image/.test(file.type)) {
-                fileInput.value='';
-                cb(file);
-              } else {
-                this.setState({
-                  status: 3,
-                  message: "Only support upload image"
-                });
-              }
-            };
-            this.container.append(fileInput);
-          }
+    shouldComponentUpdate(nextProps, nextState) {
+        if (!_.isEqual(nextState, this.state)) {
+            return true;
+        }
+        if (!this.isEqualValue(nextProps.value, this.state.value)) {
+            return true;
+        }
 
-          fileInput.click();
+        return false;
+    }
+
+    isEqualValue(v1, v2) {
+        if (v1.ops && v2.ops && _.isEqual(v1.ops, v2.ops)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    componentDidUpdate(preProps, preState) {
+        if (!this.isEqualValue(this.props.value, preState.value)) {
+            this.setContents(this.props.value);
+        }
+    }
+
+    setContents(value) {
+        let editor = this.quillEditor;
+        let range = editor.getSelection();
+        editor.setContents(value);
+        if (range && editor.hasFocus()) this.setSelection(editor, range);
+    }
+
+    setSelection(editor, range) {
+        if (range) {
+            let length = editor.getLength();
+            range.index = Math.max(0, Math.min(range.index, length - 1));
+            range.length = Math.max(0, Math.min(range.length - 1, length - 1 - range.index));
+        }
+        editor.setSelection(range);
+    }
+
+    onTextHandler(delta) {
+        let value = this.quillEditor.getContents();
+        this.setState({
+            value: value,
+        }, () => {
+            this.props.onTextChange && this.props.onTextChange(value);
+        });
+    }
+
+    imageHandler(cb) {
+        return function () {
+            let fileInput = this.container.querySelector("input.ql-image[type=file]");
+            if (!fileInput) {
+                fileInput = document.createElement("input");
+                fileInput.setAttribute("type", "file");
+                fileInput.classList.add("ql-image");
+                fileInput.setAttribute("accept", "image/png, image/gif, image/jpeg, image/bmp, image/x-icon");
+                fileInput.onchange = () => {
+                    let file = fileInput.files[0];
+                    if (/^image/.test(file.type)) {
+                        fileInput.value = '';
+                        cb(file);
+                    } else {
+                        this.setState({
+                            status: 3,
+                            message: "Only support upload image"
+                        });
+                    }
+                };
+                this.container.append(fileInput);
+            }
+
+            fileInput.click();
         };
     }
 
@@ -93,12 +151,12 @@ export default class QuillEditor extends React.Component {
         let fd = new FormData();
         fd.append("image", file);
         fetchData(
-          "/api/upload/editor",
-          {
-            method: "POST",
-            body: fd
-          },
-          [this.uploadSuccess, this.uploadFailed]
+            "/api/upload/editor",
+            {
+                method: "POST",
+                body: fd
+            },
+            [this.uploadSuccess, this.uploadFailed]
         );
     }
 
--- client/routes/Editor/index.js
+++ client/routes/Editor/index.js
@@ -1,48 +1,71 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Map, List,fromJS } from 'immutable';
 import TagControl from '../../controls/TagControl';
 import QuillEditor from '../../controls/QuillEditor';
 
 
 class Editor extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            status:0,
-            formData:{
-                tags:[],
-            },
-            message:'',
+        this.state = {
+            status: 0,
+            formData: Map({
+                title:'',
+                tags: List([]),
+                content:Map(),
+            }),
+            message: '',
         };
         this.handleTagsChange = this.handleTagsChange.bind(this);
+        this.handleTitleChange =this.handleTitleChange.bind(this);
+        this.handleEditorChange = this.handleEditorChange.bind(this);
     }
 
 	handleTagsChange(ids){
 		let formData = this.state.formData;
-		formData.tags = ids;
+        let idList = List(ids);
+        formData.update('tags',idList);
 		this.setState({
 			formData:formData,
 		});
 	}
 
-    render() {
+    handleTitleChange(event){
+        let value = event.target.value;
+        let formData = this.state.formData.set('title',value);
+        this.setState({
+            formData:formData,
+        });
+    }
+
+    handleEditorChange(value) {
+        let content = fromJS(value);
         let formData = this.state.formData;
+        let newFd = formData.update('content', val => val.mergeDeep(content));
+        this.setState({
+            formData: newFd,
+        });
+    }
+
+    render() {
+        let formData = this.state.formData.toJS();
 
         return (
             <div className="view-editor">
                 <form>
                     <div className="form-group">
-                        <input type="text" className="form-control" placeholder="input article title..." />
-                    </div>
+                        <input type="text" onChange={this.handleTitleChange} className="form-control" value={formData.title} placeholder="input article title..." />
+                    </div>  
                     <div className="form-group">
                         <TagControl onValueChange={this.handleTagsChange} selected={formData.tags} tags={this.props.tag.tags} />
                     </div>
-                    <QuillEditor />
+                    <QuillEditor onTextChange={this.handleEditorChange} value={formData.content}/>
                     <div className="publish-footer">
                         <div className="container">
                             <div className="operations clearfix">
                                 <div className="float-right">
-                                    <button className="btn btn-primary" type="submit">Button</button>
+                                    <button className="btn btn-primary" type="submit">Publish</button>
                                 </div>
                             </div>
                         </div>
